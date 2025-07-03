8cb28d3b219cc224c9199c27896a20f96efb4d66
Luis-Rojas-H <larojash@uni.pe>
2025-07-02 23:38:58 -0500

    main force

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ def main():
 @app.route("/practice")
 def practice():
     return render_template('main_html.html')
+<<<<<<< HEAD
 
+@app.route("/filtro")
+def filtro():
+    return render_template('main_html_face.html')
+
+=======
+@app.route("/camara-trasera")
+def camaratrasera():
+    return render_template('trasera.html')
+>>>>>>> c2739cc18849551f55282d244d3e7ea738967a30
 
 if __name__ == "__main__":
     app.run()
